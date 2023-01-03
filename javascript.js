225fa54617f88da5cc6abfeab0e918a6bfa52111
BraedonBarham <bbraedon10@gmail.com~>
2023-01-02 21:58:36 -0700

    Establish project on GitHub

---
+++ javascript.js
@@ -0,0 +1,82 @@
+
+function computerChoice(){
+    let getComputerChoice = Math.ceil(Math.random()*3);
+    if (getComputerChoice == 1){
+        getComputerChoice = "rock";
+    }else if (getComputerChoice == 2){
+        getComputerChoice = "paper";
+    }
+    else{
+        getComputerChoice = "scissors";
+    }
+    return getComputerChoice;
+}
+
+console.log(computerChoice())
+
+
+function getPlayerSelection(){
+    let selection = prompt().toLowerCase()
+    selection = selection
+    return getPlayerSelectionValid(selection);
+}
+
+function getPlayerSelectionValid(selection){
+    if (selection == "rock" || selection == "paper" || selection == "scissors"){
+        return selection
+    }
+    else{
+        console.log("Input correct object!!!")
+        getPlayerSelection()
+    }
+
+}
+
+function gameLogic(playerSelection, getComputerChoice){
+
+    if (playerSelection === getComputerChoice){
+        console.log("Tie - Getting new selections!");
+        return gameLogic(getPlayerSelection(), computerChoice())
+    }
+
+    else if (playerSelection == "rock" && getComputerChoice == "scissors"){
+        return true;
+    }
+    else if (playerSelection == "paper" && getComputerChoice == "rock"){
+        return true;
+    }
+    else if (playerSelection == "scissors" && getComputerChoice == "paper"){
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+
+function playRound(){
+    let winner;
+    let playerCount = 0;
+    let computerCount = 0;
+    for (let i = 0; i < 5; i++){
+    winner = gameLogic(getPlayerSelection(), computerChoice())
+    if (winner){
+        playerCount++;
+    }
+    else{
+        computerCount++;
+    }
+
+
+  }
+  console.log(`Player has ${playerCount} wins, and Computer has ${computerCount} wins!`)
+}
+
+
+function game(){
+    playRound()
+
+
+
+}
+game()
